b38969eff6ba7653df28c326c3062b6973910069
hornuglan <hornuglan@gmail.com>
2019-01-18 20:29:41 +0300

    react component was modified

--- src/js/ImagesRender.js
+++ src/js/ImagesRender.js
@@ -1,3 +1,16 @@
 import React from 'react';
+import { render }  from 'react-dom';
+import axios from  'axios';
 
-const 
+import '../css/style.css';
+
+class ImagesRender extends React.Component {
+    render() {
+        return (
+            <div>Test</div>
+        );
+    }
+}
+
+export default 'ImagesRender';
+
